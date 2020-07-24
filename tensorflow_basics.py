75f7aa42b0e0fe9f69c6733623fd78a7d55e54dd
vitalwarley <vitalwarley@gmail.com>
2020-07-24 14:00:08 -0300

    tf-guide: adds TODO for variable_scope

--- tensorflow_basics.py
+++ tensorflow_basics.py
@@ -24,22 +24,22 @@
 # - Experiment 1: "Hello, TensorFlow".
 # - Experiment 2: Understand functions of sessions through a session experiment using the with session function.
 # - Experiment 3: Understand matrix multiplication by multiplying two matrices with ranks of tensors greater than 2.
-# - Experiment 4: Understand the definition of variables. 
+# - Experiment 4: Understand the definition of variables.
 #   - Define variables with Variable and get_variable respectively and observe the difference between these two methods.
-# - Experiment 5: Understand the visualization of TensorBoard. 
-#   - TensorBoard aggregates all kinds of data into a log file. 
+# - Experiment 5: Understand the visualization of TensorBoard.
+#   - TensorBoard aggregates all kinds of data into a log file.
 #   - You can enable TensorBoard service to read the log file and enable the 6060 port to provide web services so that users can view data via a browser.
 # - Experiment 6: Understand data reading and processing by reading .csv files and displaying them based on given conditions.
-# - Experiment 7: Understand graphic operations. 
+# - Experiment 7: Understand graphic operations.
 #   - Create a graph in three ways and set it as the default graph. Use the get_default_graph() function to access the default graph and verify its settings.
-# - Experiment 8: Understand save and use of models. 
-#   - After importing data, analyze data characteristics and define variables based on the characteristics. 
-#   - Create a model and define output nodes. 
-#   - Build the structure for forward propagation and then the structure for backpropagation. 
-#   - Compile and train the model to get appropriate parameters. 
-#   - After training data and testing the model, create a saver and a path to save parameters in the session automatically. 
+# - Experiment 8: Understand save and use of models.
+#   - After importing data, analyze data characteristics and define variables based on the characteristics.
+#   - Create a model and define output nodes.
+#   - Build the structure for forward propagation and then the structure for backpropagation.
+#   - Compile and train the model to get appropriate parameters.
+#   - After training data and testing the model, create a saver and a path to save parameters in the session automatically.
 #   - When the model is saved, you can access the model for use.
-# - Experiment 9: A comprehensive experiment of forecasting housing price through the instantiation of linear regression. 
+# - Experiment 9: A comprehensive experiment of forecasting housing price through the instantiation of linear regression.
 #   - Use the dataset of housing prices in Beijing and skills in the prior eight experiments to forecast the housing price.
 
 # %% [markdown]
@@ -49,11 +49,11 @@ import tensorflow as tf
 
 # %%
 # Defining a variable
-hello = tf.constant('hello, tensorflow!')  # a constant 
+hello = tf.constant("hello, tensorflow!")  # a constant
 
 
 # %%
-sess = tf.Session()     # Creates a session
+sess = tf.Session()  # Creates a session
 print(sess.run(hello))  # Run the session on the `hello` constant to get the result
 # %%
 sess.close()  # Close the session
@@ -61,7 +61,7 @@ sess.close()  # Close the session
 # ## Experiment 2
 # - After this experiment you will understand the definition of sessions and how to use them with the python context manager (`with`).
 
-# %% 
+# %%
 import tensorflow as tf
 
 # %%
@@ -72,8 +72,8 @@ b = tf.constant(4)
 # %%
 # Creating a Session
 with tf.Session() as sess:  # `with` starts a context where Session will be automatically closed
-    print(f'Add: {sess.run(a + b)}')
-    print(f'Multiply: {sess.run(a * b)}')
+    print(f"Add: {sess.run(a + b)}")
+    print(f"Multiply: {sess.run(a * b)}")
 
 
 # %% [markdown]
@@ -118,25 +118,32 @@ import tensorflow as tf
 tf.reset_default_graph()
 
 # %%
-var1 = tf.Variable(10.0, name='varname')
-var2 = tf.Variable(11.0, name='varname')
+var1 = tf.Variable(10.0, name="varname")
+var2 = tf.Variable(11.0, name="varname")
 var3 = tf.Variable(12.0)
 var4 = tf.Variable(13.0)
 
 # %%
-with tf.variable_scope('test1'):
-    var5 = tf.get_variable('varname', shape=[2], dtype=tf.float32)
+# Variable scope allows you to create new variables and to share already created ones 
+# while providing checks to not create or share by accident.
+# TODO: make it more clear
+with tf.variable_scope("test1"):
+    var5 = tf.get_variable("varname", shape=[2], dtype=tf.float32)
 
-with tf.variable_scope('test2'):
-    var6 = tf.get_variable('varname', shape=[2], dtype=tf.float32)
+with tf.variable_scope("test2"):
+    var6 = tf.get_variable("varname", shape=[2], dtype=tf.float32)
 
 # %%
-print('var1: ', var1.name)
-print('var2: ', var2.name)  # A tf variable with a existing name gets a suffix to differentiate between them
-print('var3: ', var3.name)
-print('var4: ', var4.name)
-print('var5: ', var5.name)  # With `variable_scope` we can enclose a variable within a desired scope
-print('var6: ', var6.name)
+print("var1: ", var1.name)
+print(
+    "var2: ", var2.name
+)  # A tf variable with a existing name gets a suffix to differentiate between them
+print("var3: ", var3.name)
+print("var4: ", var4.name)
+print(
+    "var5: ", var5.name
+)  # With `variable_scope` we can enclose a variable within a desired scope
+print("var6: ", var6.name)
 # %% [markdown]
 # ## Experiment 5
 
@@ -151,4 +158,3 @@ print('var6: ', var6.name)
 
 # %% [markdown]
 # ## Experiment 9
-
